b3cd21b90edff392b43cb7db71fab541f60e9e0a
antiKk <4548266+antiKk@users.noreply.github.com>
2026-04-17 22:19:03 +1000

    Bump version to 0.8
    Added package.sh to package a .muxapp

---
+++ package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+VERSION=$(grep -oP '(?<=#define VTREE_VERSION ")[^"]+' main.c)
+FOLDER="vTree Gold"
+ARCHIVE="vTree Gold ${VERSION}"
+
+echo "Packaging vTree Gold v${VERSION}..."
+
+rm -rf "$FOLDER"
+mkdir -p "$FOLDER"
+
+cp vtree config.ini theme.ini "$FOLDER/"
+cp -r res fonts "$FOLDER/"
+
+zip -r "${ARCHIVE}.zip" "$FOLDER"
+mv "${ARCHIVE}.zip" "${ARCHIVE}.muxapp"
+rm -rf "$FOLDER"
+
+echo "Done: ${ARCHIVE}.muxapp"
